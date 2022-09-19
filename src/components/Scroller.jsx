6706c739c0adb97847eeaa960102a7daf90eae60
João Correia <jpcorreia@mail.com>
2022-09-19 14:30:06 +0100

    6

--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -8,28 +8,26 @@ import {BsFillPersonLinesFill} from 'react-icons/bs'
 const Scroller = () => {
   return (
     <div className='flex flex-col items-center justify-center mb-8 bg-transparent' id='scroller'>
-
     <HashLink to='#' >
-     <div className='p-6 m-1 mt-4 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
+     <div className='p-6 m-1 mt-16 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
       <HiOutlineChevronDoubleUp size={30} />
      </div>
     </HashLink>
     <div className='flex justify-start py-1 items-center'>
-      
-                    <div className='rounded-full bg-[#67E8F9]  shadow-[#67E8F9]  p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
-                      <FaLinkedinIn size={24} color={'#121212'}/>
-                    </div>
-                    <div className='rounded-full bg-[#67E8F9]  shadow-[#67E8F9] p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
-                      <FaGithub size={24} color={'#121212'}/>
-                    </div>
-                    <div className='rounded-full bg-[#67E8F9] color-[#121212] shadow-[#67E8F9] p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
-                      <AiOutlineMail size={24} color={'#121212'}/>
-                    </div>
-                    <div className='rounded-full bg-[#67E8F9]  shadow-[#67E8F9] p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
-                      <BsFillPersonLinesFill size={24} color={'#121212'}/>
-                    </div>
-                  </div>
-                  <p className='mt-4 '>© 2022 João Correia</p>
+      <div className='rounded-full bg-[#ffffff]  shadow-[#67E8F9]  p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
+        <FaLinkedinIn size={24} color={'#121212'}/>
+      </div>
+      <div className='rounded-full bg-[#ffffff]  shadow-[#67E8F9] p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
+        <FaGithub size={24} color={'#121212'}/>
+      </div>
+      <div className='rounded-full bg-[#ffffff] color-[#121212] shadow-[#67E8F9] p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
+        <AiOutlineMail size={24} color={'#121212'}/>
+      </div>
+      <div className='rounded-full bg-[#ffffff]  shadow-[#67E8F9] p-2 m-2 cursor-pointer hover:scale-110 hover:text-[#67E8F9] ease-in duration-100'>
+        <BsFillPersonLinesFill size={24} color={'#121212'}/>
+      </div>
+    </div>
+    <p className='mt-4 '>© 2022 João Correia</p>
   </div>
   )
 }
